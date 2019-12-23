0ef7c51fe49cc7c8ece79d0983912e4c29349f08
carlosjdd <39379601+carlosjdd@users.noreply.github.com>
2019-12-23 17:48:45 +0100

    Integrate ROS with blynk code

--- src/event_VIRTUAL_WRITE.py
+++ src/event_VIRTUAL_WRITE.py
@@ -1,27 +1,7 @@
-"""
-Blynk is a platform with iOS and Android apps to control
-Arduino, Raspberry Pi and the likes over the Internet.
-You can easily build graphic interfaces for all your
-projects by simply dragging and dropping widgets.
+#!/usr/bin/python
 
-  Downloads, docs, tutorials: http://www.blynk.cc
-  Sketch generator:           http://examples.blynk.cc
-  Blynk community:            http://community.blynk.cc
-  Social networks:            http://www.fb.com/blynkapp
-                              http://twitter.com/blynk_app
-
-This example shows how to perform custom actions
-using data from the widget.
-
-In your Blynk App project:
-  Add a Slider widget,
-  bind it to Virtual Pin V3.
-  Run the App (green triangle in the upper right corner)
-
-It will automagically call v3_write_handler.
-In the handler, you can use args[0] to get current slider value.
-"""
-
+import rospy
+from std_msgs.msg import Bool
 import BlynkLib
 
 BLYNK_AUTH = 'gASisGlaGebx-uTQn6zKU0H1Fw45v4aj'
@@ -39,8 +19,14 @@ def v3_slicer(value):
 
 @blynk.on("V4")
 def v4_button(value):
-    x = value[0]
-    print(x)
+    if value[0] == 0:
+        aux = False
+    else:
+        aux = True
+    pub.publish(aux)
 
-while True:
-    blynk.run()
+if __name__ == '__main__':
+    pub = rospy.Publisher('switch_printer', Bool, queue_size=10)
+    rospy.init_node('blynk_printer', anonymous=True)
+    while True:
+        blynk.run()
