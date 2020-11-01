2a14f6d501950486dee933d9ceaff6098d1c4657
carlosjdd <carlosjdd@users.noreply.github.com>
2020-11-01 17:38:48 +0100

    Create printer node with ROS

--- src/printer.py
+++ src/printer.py
@@ -1,39 +1,84 @@
 #!/usr/bin/python
 
+# import the necessary packages
 import rospy
-from std_msgs.msg import Bool
 import BlynkLib
 
-BLYNK_AUTH = 'gASisGlaGebx-uTQn6zKU0H1Fw45v4aj'
+# import the necessary msgs. Example with msg type String_Int_Arrays:
+from std_msgs.msg import Bool
 
-# Initialize Blynk
-blynk = BlynkLib.Blynk(BLYNK_AUTH)
+class printer():
+    """ Class class_name.
 
-# Register virtual pin handler
-@blynk.on("V3")
-#def v3_write_handler(value):
-#    print('Current slider value: {}'.format(value[0]))
-def v3_slicer(value):
-    x = value[0]
-    print(x)
+    Info about the class
+    """
 
-@blynk.on("V4")
-def v4_button(value):
-    print ("Han pulsado el boton")
-    print(value[0])
-    x = value[0]
-    if x == "0":
-        print ("El valor es 0")
-        aux = False
-    else:
-        aux = True
-        print ("El valor es 1")
-    print("enviar topic")
-    pub.publish(aux)
-    print("topic enviado")
+    def __init__(self):
+        """Class constructor
 
-if __name__ == '__main__':
-    rospy.init_node('blynk_printer', anonymous=True)
-    pub = rospy.Publisher('switch_printer', Bool, queue_size=10)
-    while True:
-        blynk.run()
+        It is the constructor of the class. It does:
+        """
+
+        #Subscribe to ROS topics
+        self.printer_pub = rospy.Publisher('switch_printer', Bool, queue_size=10)
+
+        self.configuration()
+
+        print("[INFO] Node started")
+
+    def configuration(self):
+        BLYNK_AUTH = 'gASisGlaGebx-uTQn6zKU0H1Fw45v4aj'
+
+        # Initialize Blynk
+        self.blynk = BlynkLib.Blynk(BLYNK_AUTH)
+
+
+    def run_loop(self):
+        """ Infinite loop.
+
+        When ROS is closed, it exits.
+        """
+        while not rospy.is_shutdown():
+            #functions to repeat until the node is closed
+            self.blynk.run()
+
+    # Register virtual pin handler
+    @self.blynk.on("V3")
+    def v3_slicer(self, value):
+        x = value[0]
+        print(x)
+
+    @self.blynk.on("V4")
+    def v4_button(self, value):
+        print ("Han pulsado el boton")
+        print(value[0])
+        x = value[0]
+        if x == "0":
+            print ("El valor es 0")
+            aux = False
+        else:
+            aux = True
+            print ("El valor es 1")
+
+        self.printer_pub.publish(aux)
+
+
+if __name__=='__main__':
+    """ Main void.
+
+    Is the main void executed when started. It does:
+    - Start the node
+    - Create an object of the class
+    - Run the node
+
+    """
+    try:
+        rospy.init_node('blink_printer')       # Init ROS node
+
+        blynk_printer = class_name()
+        rospy.on_shutdown(blynk_printer.stopping_node)   #When ROS is closed, this void is executed
+
+        blynk_printer.run_loop()
+
+    except rospy.ROSInterruptException:
+        pass
